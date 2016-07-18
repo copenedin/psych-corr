ae51220c4b3e405a8d926091b1115c3a598b89fd
Edin Ikanovic <edinikanovic@gmail.com>
2016-07-18 10:31:50 +0200

    Added access to questionaire answers

--- test_psych_corr.py
+++ test_psych_corr.py
@@ -1,10 +1,22 @@
 import pandas as pd
+import pickle
 
-predict = pd.read_csv('../plot_entropy_rate_dt/predictability_dist_only_POI_NSD_DBSCAN.csv', index_col=0)
+def get_predict():
+	predict = pd.read_csv('../plot_entropy_rate_dt/predictability_dist_only_POI_NSD_DBSCAN.csv', index_col=0)
 
-col = predict.columns
-usernames = []
-for c in col:
-	usernames.append(c[:-4])
+	col = predict.columns
+	usernames = []
+	for c in col:
+		usernames.append(c[:-4])
 
-predict.columns = usernames
+	predict.columns = usernames
+	return predict
+
+def get_questionare_answers():
+	with open('/lscr_paper/amoellga/Data/Telefon/questiondic.p', 'rb') as f:
+		answers = pickle.load(f)
+	return answers
+
+predict = get_predict()
+usernames = predict.columns
+answers = get_questionare_answers()
